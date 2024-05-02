15877292de855817f0eee548215f61e9777bf754
Heitor Cazado <heitorcazado@gmail.com>
2024-05-02 11:38:34 -0300

    w1 fixing addtocart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let customerCart = getLocalStorage("so-cart");
+  if (customerCart == null) {
+    customerCart = [];
+    customerCart.push(product);
+  } else {
+    customerCart.push(product);
+  }
+  setLocalStorage("so-cart", customerCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
